e643811856a48fcc1659a0d0f865817bd3700967
Alex Pletzer <alexander@gokliya.net>
2023-02-28 16:45:53 +1300

    working on one function

--- paleoscripts/__init__.py
+++ paleoscripts/__init__.py
@@ -3,3 +3,7 @@
 __author__ = """Alexander Pletzer"""
 __email__ = 'alexander.pletzer@nesi.org.nz'
 __version__ = '0.1.0'
+
+__all__ = ('paleoscripts',)
+
+from .paleoscripts import pad_data
--- paleoscripts/paleoscripts.py
+++ paleoscripts/paleoscripts.py
@@ -1,1 +1,22 @@
-"""Main module."""
+import xarray as xr
+import numpy as np
+
+def pad_data(data_array):
+    """
+    Pad the data along the last axis (longitudes)
+    :param data_array: instance of xarray.DataArray
+    :returns a new DataArray with an additional row, containing the first row values
+    """
+
+    # extended shape, last dimension is lon
+    x_shape = list(data_array.shape)
+    x_shape[-1] += 1
+
+    x_data = np.empty(x_shape, data_array.dtype())
+    x_data[..., :-2] = data_array[:]
+    x_data[..., -1] = x_data[..., 0]
+
+    x_data_array = xr.DataArray(x_data, coords=data_array.coords[:-1] + [x_lon], \
+                                dims=data_array.dims, name=data_array.name, attrs=data_array.attrs)
+    return x_data_array
+
