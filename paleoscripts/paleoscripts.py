977528e3de80fbdb2feb3707b31592d7427cefa0
Alex Pletzer <alexander@gokliya.net>
2023-03-01 13:38:08 +1300

    added contourplot

--- paleoscripts/paleoscripts.py
+++ paleoscripts/paleoscripts.py
@@ -1,11 +1,17 @@
 import xarray as xr
 import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.feature as cfeature
+import cartopy.crs as ccrs
+import geocat.viz as gv
+from cartopy.mpl.gridliner import LongitudeFormatter, LatitudeFormatter
+
 
 def apply_cyclic_padding(data_array: xr.DataArray, coord_name: str='longitude', period: float=360.) -> xr.DataArray:
     """
-    Apply cyclic padding to the data along a coordinate
+    Apply cyclic padding to a data array along a given coordinate
     :param data_array: instance of xarray.DataArray
-    :param cooord_name: the coordinate name
+    :param coord_name: the coordinate name
     :param period: periodicity length
     :returns a new DataArray with an additional row, containing the first row values
     """
@@ -34,7 +40,6 @@ def apply_cyclic_padding(data_array: xr.DataArray, coord_name: str='longitude', 
                                 name=data_array.name,\
                                 attrs=data_array.attrs)
 
-
     # fill in the values of the new data array
     slab = [slice(None, None) for i in range(index_coord)] + \
            [slice(0, len(coord))] + \
@@ -54,4 +59,73 @@ def apply_cyclic_padding(data_array: xr.DataArray, coord_name: str='longitude', 
     x_data_array[tuple(slab_end)] = x_data_array[tuple(slab_beg)]
 
     return x_data_array
+
+
+def create_contourf_plot(data_array, title='', units='', figsize=(12, 8)):
+
+    fig = plt.figure(figsize=figsize)
+    ax = plt.axes(projection=ccrs.PlateCarree(central_longitude= -60))
+
+    # add land feature
+    ax.add_feature(cfeature.LAND, facecolor="lightgrey", zorder=1)
+
+
+    gv.set_titles_and_labels(ax,
+                             maintitle="",
+                             maintitlefontsize=18,
+                             lefttitle="",
+                             lefttitlefontsize=18,
+                             righttitle="",
+                             righttitlefontsize=18)
+
+    # Format tick labels as latitude and longitudes
+    gv.add_lat_lon_ticklabels(ax=ax)
+
+    # Use geocat-viz utility function to customize tick marks
+    gv.set_axes_limits_and_ticks(ax,
+                                 xlim=(-90, 120),
+                                 ylim=(0, 90),
+                                 xticks=(-90, -60, -30, 0, 30, 60, 90, 120),
+                                 yticks=(0, 30, 60, 90))
+
+    # Remove degree symbol from tick labels
+    ax.yaxis.set_major_formatter(LatitudeFormatter(degree_symbol=''))
+    ax.xaxis.set_major_formatter(LongitudeFormatter(degree_symbol=''))
+
+    # Add minor tick marks
+    gv.add_major_minor_ticks(ax,
+                             x_minor_per_major=4,
+                             y_minor_per_major=4,
+                             labelsize=14)
+
+    # Make sure that tick marks are only on the left and bottom sides of subplot
+    ax.tick_params('both', which='both', top=False, right=False)
+
+
+    vmo_plot = data_array.plot.contourf(
+                                 ax=ax,
+                                 transform=ccrs.PlateCarree(),
+                                 levels=np.linspace(200,320,60),
+                                 cmap="bwr",
+                                 add_colorbar=False)  
+
+    # Add and customize colorbar
+    cbar_ticks = np.arange(200, 320, 20)
+    plt.colorbar(ax=ax,
+             mappable=vmo_plot,
+             extendrect=False,
+             extendfrac='Auto',
+             label=units,
+             ticks=cbar_ticks,
+             drawedges=False,
+             aspect=15,
+             shrink=0.55)
+
+    ax.coastlines()
+    ax.set(xlabel=None)
+    ax.set(ylabel=None)
+
+    plt.title(title)
+
+    return vmo_plot
 
--- tests/test_paleoscripts.py
+++ tests/test_paleoscripts.py
@@ -7,23 +7,40 @@ import pytest
 import paleoscripts
 import xarray as xr
 import numpy as np
+import matplotlib.pyplot as plt
 
-def test_apply_cyclic_padding():
 
-    # number of cells
-    nlat, nlon = 3, 4
+def create_latlon_data(nlat, nlon):
     nlat1, nlon1 = nlat + 1, nlon + 1
 
-    # create a data array of lat-lon
+    # create a data array with lat-lon cooordinates
     lat = np.linspace(-90., 90, nlat1)
 
+    # create a lon coordinate, the last value (ie 360 deg) is missing
     dlon = 360/nlon
     lon = np.linspace(0., 360 - dlon, nlon)
 
+    # create some data
     data = np.arange(0, nlat1 * nlon).reshape((nlat1, nlon))
-
     da = xr.DataArray(data, coords=[lat, lon], dims=['latitude', 'longitude'], name='temperature')
 
+    return da
+
+def test_apply_cyclic_padding():
+
+    nlat, nlon = 3, 4
+    da = create_latlon_data(nlat, nlon)
     x_da = paleoscripts.apply_cyclic_padding(da)
 
     assert np.all(x_da[..., 0] == x_da[..., -1])
+
+
+def test_create_contourf_plot():
+    
+    nlat, nlon = 10, 20
+    da = create_latlon_data(nlat, nlon)
+    x_da = paleoscripts.apply_cyclic_padding(da)
+    p = paleoscripts.create_contourf_plot(da, title='test', units='m/s')
+    plt.show()
+
+
